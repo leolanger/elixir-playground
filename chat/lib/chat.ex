52032e1de5ec17606ba6df0c66be9d275c2e2728
leolanger <leolanger2001@outlook.com>
2020-10-24 00:03:00 +0800

    do test

---
+++ chat/lib/chat.ex
@@ -0,0 +1,27 @@
+defmodule Chat do
+  def receive_message(message) do
+    :timer.sleep(100)
+    IO.puts(message)
+  end
+
+  def send_message(:moebi@localhost, message) do
+    :timer.sleep(100)
+    spawn_task(__MODULE__, :receive_message_for_moebi, :moebi@localhost, [message, Node.self()])
+  end
+
+  def receive_message_for_moebi(message, from) do
+    IO.puts(message)
+    send_message(from, "chicken?")
+  end
+
+  def spawn_task(module, fun, recipient, args) do
+    recipient
+    |> remote_supervisor()
+    |> Task.Supervisor.async(module, fun, args)
+    |> Task.await()
+  end
+
+  defp remote_supervisor(recipient) do
+    {Chat.TaskSupervisor, recipient}
+  end
+end
